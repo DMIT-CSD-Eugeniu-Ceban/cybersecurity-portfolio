7b07fae7fdfc9f2af52b9c1b63fd63512d64af5d
Eugeniu Ceban <83615676+DMIT-CSD-Eugeniu-Ceban@users.noreply.github.com>
2025-12-25 15:59:32 -0700

    Update 02-specific-date-investigation.sql

--- Cybersecurity-Portfolio/06-sql-security-analysis/queries/02-specific-date-investigation.sql
+++ Cybersecurity-Portfolio/06-sql-security-analysis/queries/02-specific-date-investigation.sql
@@ -1,1 +1,7 @@
+-- Investigate login attempts on specific dates related to a suspicious incident
+
+SELECT *
+FROM log_in_attempts
+WHERE login_date = '2022-05-09'
+   OR login_date = '2022-05-08';
 
