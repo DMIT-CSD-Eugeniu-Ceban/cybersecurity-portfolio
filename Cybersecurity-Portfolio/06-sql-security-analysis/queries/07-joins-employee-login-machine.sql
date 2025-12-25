12e90cbe160c30aa578171655191e034eb4050d3
Eugeniu Ceban <83615676+DMIT-CSD-Eugeniu-Ceban@users.noreply.github.com>
2025-12-25 16:00:35 -0700

    Add query for employee-machine-login correlation
    Add SQL query to correlate employees with their machines and login attempts.

--- Cybersecurity-Portfolio/06-sql-security-analysis/queries/07-joins-employee-login-machine.sql
+++ Cybersecurity-Portfolio/06-sql-security-analysis/queries/07-joins-employee-login-machine.sql
@@ -1,1 +1,7 @@
 
+-- Correlate employees, their machines, and login attempts
+
+SELECT e.name, e.department, m.hostname, l.login_date, l.login_time, l.success
+FROM employees e
+INNER JOIN machines m ON e.machine_id = m.machine_id
+INNER JOIN log_in_attempts l ON e.employee_id = l.employee_id;
