13af7884de918da49d9e4b59fc619fb82fa3bd22
Eugeniu Ceban <83615676+DMIT-CSD-Eugeniu-Ceban@users.noreply.github.com>
2025-12-25 17:45:27 -0700

    Update allowlist_updater.py

--- Cybersecurity-Portfolio/09-automate-cybersecurity-tasks-with-python/src/allowlist_updater.py
+++ Cybersecurity-Portfolio/09-automate-cybersecurity-tasks-with-python/src/allowlist_updater.py
@@ -1,1 +1,26 @@
 
+---
+
+## `09-automate-cybersecurity-tasks-with-python/src/allowlist_updater.py`
+
+```python
+allow_file = "data/allow_list.txt"
+remove_file = "data/remove_list.txt"
+
+with open(allow_file, "r") as file:
+    allow_list = file.read().split()
+
+with open(remove_file, "r") as file:
+    remove_list = file.read().split()
+
+for ip in remove_list:
+    if ip in allow_list:
+        allow_list.remove(ip)
+
+updated_allow_list = "\n".join(allow_list)
+
+with open(allow_file, "w") as file:
+    file.write(updated_allow_list)
+
+print("Allow list updated successfully.")
+
