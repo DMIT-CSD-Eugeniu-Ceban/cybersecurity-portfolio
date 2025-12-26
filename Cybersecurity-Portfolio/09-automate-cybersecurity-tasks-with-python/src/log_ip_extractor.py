dba8dbed75ce98ca05633436ccaa617a784ae6f6
Eugeniu Ceban <83615676+DMIT-CSD-Eugeniu-Ceban@users.noreply.github.com>
2025-12-25 17:45:45 -0700

    Update log_ip_extractor.py

--- Cybersecurity-Portfolio/09-automate-cybersecurity-tasks-with-python/src/log_ip_extractor.py
+++ Cybersecurity-Portfolio/09-automate-cybersecurity-tasks-with-python/src/log_ip_extractor.py
@@ -1,1 +1,20 @@
+import re
+
+log_file = "data/sample_auth.log"
+flagged_ips = {"203.0.113.5", "198.51.100.77"}
+
+with open(log_file, "r") as file:
+    log_data = file.read()
+
+ip_pattern = r"\b(?:\d{1,3}\.){3}\d{1,3}\b"
+found_ips = re.findall(ip_pattern, log_data)
+
+unique_ips = set(found_ips)
+
+print("Extracted IP addresses:")
+for ip in unique_ips:
+    if ip in flagged_ips:
+        print(f"{ip} - FLAGGED")
+    else:
+        print(ip)
 
